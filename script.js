c0fd3ef34caa8d305c33915fe9a633eb0ad8077a
unknown <dswenarton@gmail.com>
2018-10-24 13:56:16 -0400

    reorganization

--- script.js
+++ script.js
@@ -1,135 +1,8 @@
 /* @AUTHOR David Swenarton & Mingquan Liu */
 
-function video_ended() {
-    // What you want to do after the event
-    document.getElementById("playbutton").innerText = "Play"
-}
-
-
-function show_slider_value() {
-    // document.getElementById("playbutton").innerText = "Play"
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-    var mediaElement = $("#vplayer").get(0);
-    var duration = mediaElement.duration;
-    var time = parseFloat(slider.value / 100 * duration)
-    output.innerHTML = time.toFixed(2) + " / " + duration.toFixed(2)
-    mediaElement.currentTime = time
-    mediaElement.pause()
-    $("#vplayer").load()
-
-}
-
-function play_jquery() {
-    // $('#playbutton').click(function () {
-    if (!$("#vplayer").get(0).paused) {
-        $("#vplayer").get(0).pause();
-        document.getElementById("playbutton").innerText = "Play "
-    } else {
-        $("#vplayer").get(0).play();
-        document.getElementById("playbutton").innerText = "Pause"
-    }
-    $("#vplayer").load()
-    // });
-}
-
-function change_frame(isNext) {
-    var cTime = $("#vplayer").get(0).currentTime;
-    var duration = $("#vplayer").get(0).duration;
-    var changed_time;
-    if (isNext) {
-        changed_time = cTime + 5;
-    } else {
-        changed_time = cTime - 5;
-    }
-
-    if (changed_time > duration) {
-        changed_time = duration;
-    }
-    if (changed_time < 0) {
-        changed_time = 0;
-    }
-    $("#vplayer").get(0).currentTime = changed_time
-    $("#vplayer").get(0).pause();
-    document.getElementById("playbutton").innerText = "Play"
-    $("#vplayer").load()
-
-}
-
-function restart_video() {
-    // $('#restartbutton').click(function () {
-    $("#vplayer").get(0).currentTime = 0.0
-    $("#vplayer").get(0).play();
-    $("#vplayer").load()
-    // })
-}
-
-function rewind_video() {
-    //Create a variable to manage video elements
-    var mediaElement = $("#vplayer").get(0);
-
-    //Adjust playhead (currentTime) to 0 if elapsed time is less than 2 seconds else adjust it to the elapsed time minus 2 seconds
-    if (mediaElement.currentTime < 2) {
-        mediaElement.currentTime = 0;
-    } else {
-        mediaElement.currentTime = mediaElement.currentTime - 2;
-    }
-    $("#vplayer").load()
-}
-
-function resize_canvas() {
-    // $("#myCanvas").css("position", "relative");
-    // $("#myCanvas").css("width", $("#vcontainer").width());
-    // $("#myCanvas").css("height", $("#vcontainer").height());
-    // $("#myCanvas").css("top", $("#vcontainer").css('top'));
-    // $("#myCanvas").css("left", $("#vcontainer").css('left'));
-    // $("#myCanvas").css("right", $("#vcontainer").css('right'));
-    // $("#myCanvas").css("bottom", $("#vcontainer").css('bottom'));
-    //
-    // var slider = document.getElementById("myRange");
-    // var output = document.getElementById("demo");
-    //
-    // output.innerHTML = parseFloat(0).toFixed(2) + " / " + $("#vplayer").get(0).duration.toFixed(2)
-    // slider.value = 0
-
-    $("#myCanvas").css("border", "3px solid red");
-    $('#myCanvas').css('height', $('#vplayer').css('height'));
-    $('#myCanvas').css('width', $('#vplayer').css('width'));
-
-}
-
 function resize_slider() {
     $('#slider').css('width', $('#video_box').css('width'));
 }
-
-function change_slider() {
-    var time = $("#vplayer").get(0).currentTime;
-    var duration = $("#vplayer").get(0).duration;
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-    var percentage = parseFloat(time / duration).toFixed(2) * 100
-    slider.value = percentage;
-    output.innerHTML = time.toFixed(2) + " / " + duration.toFixed(2)
-}
-
-function select_label(annotation) {
-    annotation.removeClass("annotation");
-    annotation.addClass("annotation-selected");
-
-    // annotation.css('background-color', '#2ECC40');
-    // annotation.css('opacity', 1.0)
-}
-
-function deselect_label(annotation) {
-    if (annotation != null) {
-        annotation.removeClass("annotation-selected");
-        annotation.addClass("annotation");
-
-        // annotation.css('background-color', '#DDDDDD');
-    }
-}
-
-
 
 $(document).ready(function() {
     let drawmode = false;
@@ -140,6 +13,152 @@ $(document).ready(function() {
     let drawingAnnotationY = 0;
     let pageX = 11;
     let pageY = 82;
+
+    var currentFrame = 0
+    var frameDuration = 2
+    var annotationsByFrame = {};
+
+
+    function video_ended() {
+        // What you want to do after the event
+        document.getElementById("playbutton").innerText = "Play"
+    }
+
+
+    function show_slider_value() {
+        // document.getElementById("playbutton").innerText = "Play"
+        var slider = document.getElementById("myRange");
+        var output = document.getElementById("demo");
+        var mediaElement = $("#vplayer").get(0);
+        var duration = mediaElement.duration;
+        var time = parseFloat(slider.value / 100 * duration)
+        output.innerHTML = time.toFixed(2) + " / " + duration.toFixed(2)
+        mediaElement.currentTime = time
+        mediaElement.pause()
+        $("#vplayer").load()
+
+    }
+
+    function play_jquery() {
+        // $('#playbutton').click(function () {
+        if (!$("#vplayer").get(0).paused) {
+            $("#vplayer").get(0).pause();
+            document.getElementById("playbutton").innerText = "Play "
+        } else {
+            $("#vplayer").get(0).play();
+            document.getElementById("playbutton").innerText = "Pause"
+        }
+        $("#vplayer").load()
+        // });
+    }
+
+    function change_frame(isNext) {
+        var cTime = $("#vplayer").get(0).currentTime;
+        var duration = $("#vplayer").get(0).duration;
+        var changed_time;
+        if (isNext) {
+            changed_time = cTime + 5;
+        } else {
+            changed_time = cTime - 5;
+        }
+
+        if (changed_time > duration) {
+            changed_time = duration;
+        }
+        if (changed_time < 0) {
+            changed_time = 0;
+        }
+        $("#vplayer").get(0).currentTime = changed_time
+        $("#vplayer").get(0).pause();
+        document.getElementById("playbutton").innerText = "Play"
+        $("#vplayer").load()
+
+    }
+
+    function restart_video() {
+        // $('#restartbutton').click(function () {
+        $("#vplayer").get(0).currentTime = 0.0
+        $("#vplayer").get(0).play();
+        $("#vplayer").load()
+        // })
+    }
+
+    function rewind_video() {
+        //Create a variable to manage video elements
+        var mediaElement = $("#vplayer").get(0);
+
+        //Adjust playhead (currentTime) to 0 if elapsed time is less than 2 seconds else adjust it to the elapsed time minus 2 seconds
+        if (mediaElement.currentTime < 2) {
+            mediaElement.currentTime = 0;
+        } else {
+            mediaElement.currentTime = mediaElement.currentTime - 2;
+        }
+        $("#vplayer").load()
+    }
+
+    function resize_canvas() {
+        // $("#myCanvas").css("position", "relative");
+        // $("#myCanvas").css("width", $("#vcontainer").width());
+        // $("#myCanvas").css("height", $("#vcontainer").height());
+        // $("#myCanvas").css("top", $("#vcontainer").css('top'));
+        // $("#myCanvas").css("left", $("#vcontainer").css('left'));
+        // $("#myCanvas").css("right", $("#vcontainer").css('right'));
+        // $("#myCanvas").css("bottom", $("#vcontainer").css('bottom'));
+        //
+        // var slider = document.getElementById("myRange");
+        // var output = document.getElementById("demo");
+        //
+        // output.innerHTML = parseFloat(0).toFixed(2) + " / " + $("#vplayer").get(0).duration.toFixed(2)
+        // slider.value = 0
+
+        $("#myCanvas").css("border", "3px solid red");
+        $('#myCanvas').css('height', $('#vplayer').css('height'));
+        $('#myCanvas').css('width', $('#vplayer').css('width'));
+
+    }
+
+    function change_slider() {
+        var time = $("#vplayer").get(0).currentTime;
+        var duration = $("#vplayer").get(0).duration;
+        var slider = document.getElementById("myRange");
+        var output = document.getElementById("demo");
+        var percentage = parseFloat(time / duration).toFixed(2) * 100
+        slider.value = percentage;
+        output.innerHTML = time.toFixed(2) + " / " + duration.toFixed(2)
+    }
+
+    function select_label(annotation) {
+        annotation.removeClass("annotation");
+        annotation.addClass("annotation-selected");
+
+        // annotation.css('background-color', '#2ECC40');
+        // annotation.css('opacity', 1.0)
+    }
+
+    function deselect_label(annotation) {
+        if (annotation != null) {
+            annotation.removeClass("annotation-selected");
+            annotation.addClass("annotation");
+
+            // annotation.css('background-color', '#DDDDDD');
+        }
+    }
+
+    class Annotation {
+
+        constructor(user, frame, html) {
+            this.user = user;
+            this.frame = frame;
+            this.html = html;
+        }
+    }
+
+    //Attaching on action functions
+
+    $('#vplayer').on('timeupdate', change_slider);
+
+
+
 
     $('#drawbutton').on('click', function () {
         if (drawmode == true) {
@@ -166,22 +185,21 @@ $(document).ready(function() {
                 const x = e.pageX;
                 const y = e.pageY;
                 document.getElementById('debugtext').innerHTML = "(" + x + ", " + y + ")";
-                let newAnnotation = jQuery('<div/>', {
+                let annotationHtml = jQuery('<div/>', {
                     class: 'annotation',
                 });
-                // drawingAnnotation.css("top", y+'px');
-                // drawingAnnotation.css("left", x+'px');
-                newAnnotation.on('click', function() {
-                    if (newAnnotation.is(selectedAnnotation)){
+                annotationHtml.on('click', function() {
+                    if (annotationHtml.is(selectedAnnotation)){
                         deselect_label(selectedAnnotation);
                         selectedAnnotation = null;
                     } else {
                         deselect_label(selectedAnnotation);
-                        selectedAnnotation = newAnnotation;
-                        select_label(newAnnotation);
+                        selectedAnnotation = annotationHtml;
+                        select_label(annotationHtml);
                     }
                 });
-                drawingAnnotation = newAnnotation;
+                newAnnotation = new Annotation("test", 1, annotationHtml)
+                drawingAnnotation = annotationHtml;
                 drawingAnnotationX = x;
                 drawingAnnotationY = y;
                 drawingAnnotation.appendTo('#video_box');
